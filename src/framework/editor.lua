39e7dabfd117f9af461e73045388e4beefbfcd1f
roberto freire <robertofreireferrazpassos@gmail.com>
2025-08-17 13:42:29 -0300

    selected button in editor

--- src/framework/editor.lua
+++ src/framework/editor.lua
@@ -3,7 +3,8 @@ require("component")
 local spriteeditor = require("spriteeditor")
 local mapeditor = require("mapeditor")
 
-objects = {}
+buttonSelected = nil
+buttons = {}
 state = {}
 
 function change_state(st)
@@ -12,28 +13,30 @@ function change_state(st)
 end
 
 function _init()
-    objects = {}
+    buttons = {}
     _texture(10,10,"iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAMAAABHPGVmAAAAAXNSR0IArs4c6QAAAGBQTFRFAAAAIiA0RSg8Zjkxj1Y733Em2aBm7sOa+/I2meVQar4wN5RuS2kvUkskMjw5Pz90MGCCW27hY5v/X83ky9v8////m623hH6HaWpqWVZSdkKKrDIy2Vdj13u6j5dKim8w+2O8zwAAACB0Uk5TAP////////////////////////////////////////+Smq12AAAATElEQVRoge3SwQkAIQxFwTRh/60uSFYswC8eZvDgyUeIVXeMqe/RRldW7XikTzayTxJJ3IxUPLJ2sf+z45X/6WAEAAAAAAAAAAB42wdh2QY9AvdvoAAAAABJRU5ErkJggg==")
-    local spritebutton = new_button(0,11,10,0,0,0,0,10,10)
-    spritebutton.clicked = function() change_state(spriteeditor) end
-    local mapbutton = new_button(1,11,10,0,10,0,0,10,10)
-    mapbutton.clicked = function() change_state(mapeditor) end
-    add(objects,spritebutton)
-    add(objects,mapbutton)
+    spritebutton = new_button(0,11,10,0,0,0,0,10,10)
+    spritebutton.clicked = function(o) change_state(spriteeditor) buttonSelected = o end
+    mapbutton = new_button(1,11,10,0,10,0,0,10,10)
+    mapbutton.clicked = function(o) change_state(mapeditor) buttonSelected = o end
+    add(buttons,spritebutton)
+    add(buttons,mapbutton)
+    buttonSelected = spritebutton
     change_state(spriteeditor)
 end
 
 function _update()
     state:update()
-    foreach(objects, function(o)
+    foreach(buttons, function(o)
         o:update()
+        o.b.c = buttonSelected == o and 1 or 11
     end)
 end
 
 function _draw()
     state:draw()
     _rectfill(0,0,10,180,12)
-    foreach(objects, function(o)
+    foreach(buttons, function(o)
         o.b:draw()
     end)
 end
