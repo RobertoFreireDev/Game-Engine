a019633140ea5c28eb9e64008b25ea56c6c250d6
roberto freire <robertofreireferrazpassos@gmail.com>
2025-08-18 20:26:34 -0300

    Fix Grid draw

--- src/framework/mapeditor.lua
+++ src/framework/mapeditor.lua
@@ -1,6 +1,7 @@
 ﻿local mapeditor={}
 local gridIndex = 0
-local origin_x, origin_y = 15, 15
+local map_x, map_y = 15, 15
+local sprites_x, sprites_y = 15,135
 
 function mapeditor:init()
 end
@@ -10,9 +11,10 @@ end
 
 function mapeditor:draw()
     _rectfill(10,10,310,170,11)
-    _rectfill(origin_x - 1, origin_y - 1,290 + 2,100 + 2, 0)    
-    _csprc(1,0,origin_x,origin_y,3,2,29,10)
-    _cgridc(gridIndex,0,origin_x,origin_y,1,-1,10,1,1,false,false)
+    _rectfill(map_x - 1, map_y - 1,300 + 2,100 + 2, 0)
+    
+    _rectfill(sprites_x - 1, sprites_y - 1,300 + 2,40 + 2, 0)
+    _cgridc(gridIndex,0,sprites_x,sprites_y,1,-1,10,30,4,false,false)
     _print("Map Editor", 12, 2, 11)
 end
 
