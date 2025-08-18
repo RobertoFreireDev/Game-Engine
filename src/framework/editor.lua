e6568e6fe23bc342c15c64ec3ba97aa55cf81bc0
roberto freire <robertofreireferrazpassos@gmail.com>
2025-08-18 19:57:58 -0300

    Make backgroundgrid work for any color palette

--- src/framework/editor.lua
+++ src/framework/editor.lua
@@ -16,7 +16,8 @@ end
 function _init()
     buttons = {}
     _texture(0,10,10,"iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAMAAABHPGVmAAAAAXNSR0IArs4c6QAAAGBQTFRFAAAAIiA0RSg8Zjkxj1Y733Em2aBm7sOa+/I2meVQar4wN5RuS2kvUkskMjw5Pz90MGCCW27hY5v/X83ky9v8////m623hH6HaWpqWVZSdkKKrDIy2Vdj13u6j5dKim8w+2O8zwAAACB0Uk5TAP////////////////////////////////////////+Smq12AAAAl0lEQVRoge3V0Q6AIAiFYV+C93/VNiUEvStwq/3funBecBJZtXaGdLouzdAUS0sP0ac2xJ+kJOJkSCsPsbvwc5aecpcuDPmV2aTQLYmz966XYnMlfsDm9lw+ThE7Qc8Qt+1C3n3dxluKlQrVkkK05eMQW7GskHiQpVZmiPV/LZV18f5nsr1u3ggDAAAAAAAAAAAAAIDvuQBw8w7vir6ICAAAAABJRU5ErkJggg==")
-    _texture(1,10,10,"iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAMAAABHPGVmAAAAAXNSR0IArs4c6QAAAGBQTFRFAAAAIiA0RSg8Zjkxj1Y733Em2aBm7sOa+/I2meVQar4wN5RuS2kvUkskMjw5Pz90MGCCW27hY5v/X83ky9v8////m623hH6HaWpqWVZSdkKKrDIy2Vdj13u6j5dKim8w+2O8zwAAACB0Uk5TAP////////////////////////////////////////+Smq12AAAAUUlEQVRoge3RMREAAAjEMFDw/t2iAo4hQ+cOqaR7uzqZ7C+SmwkTJkyYMGHChAkTJkyYMGHChAkTJkyYMGHChAkTJkyYMGHChAkTJkyYMHlpMh2+OJAFn+v0AAAAAElFTkSuQmCC")
+    _texture(1,10,10,"iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAMAAABHPGVmAAAAAXNSR0IArs4c6QAAAGBQTFRFAAAAIiA0RSg8Zjkxj1Y733Em2aBm7sOa+/I2meVQar4wN5RuS2kvUkskMjw5Pz90MGCCW27hY5v/X83ky9v8////m623hH6HaWpqWVZSdkKKrDIy2Vdj13u6j5dKim8w+2O8zwAAACB0Uk5TAP////////////////////////////////////////+Smq12AAAAVklEQVRoge3PMQ0AMAzAsJIYf6pD0cpHZABR5s2+OWjc6MTTiacTTyeeTjydeDrxdOLpxNOJpxNPJ55OPJ14OvF04unE04mnE08nnk48nXg68XTiOTn5+qiaOAycil4AAAAASUVORK5CYII=")
+    --_pal("#272223,#f2d3ac,#e7a76c,#c28462,#905b54,#513a3d,#7a6977,#878c87,#b5c69a,#606b31,#b19e3f,#f8c65c,#d58b39,#996336,#6a422c,#b55b39")
     spritebutton = new_button(0,0,11,10,0,0,0,0,10,10)
     spritebutton.clicked = function(o) change_state(spriteeditor) buttonSelected = o end
     mapbutton = new_button(0,1,11,10,0,10,0,0,10,10)
