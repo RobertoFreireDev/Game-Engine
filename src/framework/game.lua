206c4c6ac1987cfde19985936cdcfc5be569b4db
roberto freire <robertofreireferrazpassos@gmail.com>
2025-09-07 08:48:47 -0300

    testing fire effect

--- src/framework/game.lua
+++ src/framework/game.lua
@@ -21,7 +21,8 @@ function game:init()
    _lsimg(3,_ggrid64(0,30,0,20,20))
    _lsimg(4,_ggrid64(0,50,0,20,20))
    _lsimg(5,_ggrid64(0,70,0,20,20))
-   _lsimg(6,"iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAMAAACdt4HsAAAAAXNSR0IArs4c6QAAAGBQTFRFAAAAIiA0RSg8Zjkxj1Y733Em2aBm7sOa+/I2meVQar4wN5RuS2kvUkskMjw5Pz90MGCCW27hY5v/X83ky9v8////m623hH6HaWpqWVZSdkKKrDIy2Vdj13u6j5dKim8w+2O8zwAAACB0Uk5TAP////////////////////////////////////////+Smq12AAAAUElEQVRYhe3VsQkAIBQDUbELiIKt7j+n/AXSpJO7NvDaNIU1bZs0bQW4vYBhAgAAAAAA+BFIzzW+9xg4NunaClimAroJAAAAAADgRyA917AH9ry8jfwre4oAAAAASUVORK5CYII=")
+   _lsimg(6,_ggrid64(0,90,0,20,20))
+   _lsimg(7,"iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAMAAACdt4HsAAAAAXNSR0IArs4c6QAAAGBQTFRFAAAAIiA0RSg8Zjkxj1Y733Em2aBm7sOa+/I2meVQar4wN5RuS2kvUkskMjw5Pz90MGCCW27hY5v/X83ky9v8////m623hH6HaWpqWVZSdkKKrDIy2Vdj13u6j5dKim8w+2O8zwAAACB0Uk5TAP////////////////////////////////////////+Smq12AAAAUElEQVRYhe3VsQkAIBQDUbELiIKt7j+n/AXSpJO7NvDaNIU1bZs0bQW4vYBhAgAAAAAA+BFIzzW+9xg4NunaClimAroJAAAAAADgRyA917AH9ry8jfwre4oAAAAASUVORK5CYII=")
    _stimer(0)
 end
 
@@ -36,6 +37,7 @@ function game:draw()
     _dsimgfx(4, 20, 20, self.timer,"00002000020000", color, 10, false, false)
     _dsimgfx(3, 20, 20, self.timer,"00003000020000", color, 10, false, false)
     _dsimgfx(2, 20, 20, self.timer,"00004000020000", color, 10, false, false)
+    _dsimgfx(6, 40, 20, self.timer,"03330000010000", color, 10, false, false)
 end
 
 return game
