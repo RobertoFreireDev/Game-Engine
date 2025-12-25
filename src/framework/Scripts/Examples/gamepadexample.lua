f7e9e066c695b7d48f55e996cc2759561a01ff63
roberto freire <robertofreireferrazpassos@gmail.com>
2025-12-25 08:42:14 -0300

    clean up gamepadexample

--- src/framework/Scripts/Examples/gamepadexample.lua
+++ src/framework/Scripts/Examples/gamepadexample.lua
@@ -3,19 +3,22 @@
 }
 
 function newPlayer(x,y,i)
-    local player = {
-        x = x,
-        y = y,
-        i = i,
-        c = (i + 1)*2,
-        fc = (i + 1)*3,
-        sc = (i + 1)*2,
-        pr = true,
-        r = 4,
-        speed = 1.0
-    }
+    local o = {}
+    o.x = x
+    o.y = y
+    o.i = i
+    o.fc = (i + 1)*3
+    o.sc = (i + 1)*2    
+    o.c = o.fc
+    o.pr = true
+    o.r = 4
+    o.speed = 1
+    o.butarr={1,2,0,3,5,6,3,4,8,7,4,0,1,2,0}
+    o.butarr[0]=0
+    o.dirx={-1,1, 0,0,-0.7, 0.7,0.7,-0.7}
+    o.diry={ 0,0,-1,1,-0.7,-0.7,0.7, 0.7}
 
-    function player:update()
+    function o:update()
         local dx,dy=0,0
         if _gmpd(_gamepadbuttons.DPadLeft,self.i) or _gmpd(_gamepadbuttons.RightThumbstickLeft,self.i) or _gmpd(_gamepadbuttons.LeftThumbstickLeft,self.i) then
             dx = dx - self.speed
@@ -39,11 +42,11 @@ function newPlayer(x,y,i)
         end
     end
 
-    function player:draw()    
-        _circ(player.x, player.y, player.r, player.c, 10)
+    function o:draw()    
+        _circ(self.x, self.y, self.r, self.c, 10)
     end
 
-    return player
+    return o
 end
 
 players = {}
