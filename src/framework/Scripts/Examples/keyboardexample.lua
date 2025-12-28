c6d334b7cb6117602fe299f8b59879dd5b0e69ca
roberto freire <robertofreireferrazpassos@gmail.com>
2025-12-28 20:07:25 -0300

    add Backspace on keyboard example

--- src/framework/Scripts/Examples/keyboardexample.lua
+++ src/framework/Scripts/Examples/keyboardexample.lua
@@ -8,6 +8,13 @@ function keyboardexample:init()
 end
 
 function keyboardexample:update()
+    if _btnp(_keys.Back) then
+        if #text > 0 then
+            text = text:sub(1, -2)
+        end
+        return
+    end    
+    
     for char, keycode in pairs(_keyboard) do
         if _btnp(keycode) then
             text = text ..char
