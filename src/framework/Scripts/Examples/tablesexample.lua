53e8d8989de78bbf855a698009e27a6958f5e60b
roberto freire <robertofreireferrazpassos@gmail.com>
2025-12-28 20:04:56 -0300

    add tablesexample

---
+++ src/framework/Scripts/Examples/tablesexample.lua
@@ -0,0 +1,14 @@
+﻿local tablesexample={
+    title = "Table example"
+}
+
+function tablesexample:init()
+end
+
+function tablesexample:update() 
+end
+
+function tablesexample:draw()
+end
+
+return tablesexample
