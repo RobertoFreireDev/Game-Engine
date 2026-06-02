1ad05ac0482d009d0add5e6ba5f18a1be1b2014f
Roberto Freire <RobertoF@99x.io>
2026-06-02 15:14:27 -0300

    reproduce the PICO-8 sound synthesizer

--- pocs/sfx/playpico8sfx/Program.cs
+++ pocs/sfx/playpico8sfx/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace playpico8sfx
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var sfxList = Pico8SfxParser.Parse(Files.Sfx);
+            if (sfxList.Count == 0)
+            {
+                Console.WriteLine("No SFX data could be parsed.");
+                return;
+            }
+
+            using var player = new Pico8SoundPlayer(sampleRate: 44100, channels: 1);
+            player.Play(sfxList[12], loop: true);
+
+            Console.WriteLine("Playing first PICO-8 SFX. Press Enter to stop.");
+            Console.ReadLine();
+
+            player.Stop();
+        }
+    }
+}
+
